78a0ab164204bea68099adda73e84e1cd14c187f
Aleksander <aleksander.nowak@agillic.com>
2019-08-02 14:54:20 +0200

    Add localStoreEnabled check and change update strategy

--- src/index.js
+++ src/index.js
@@ -40,4 +40,46 @@ const useLocalStorage = (key, {updateFrequency}) => {
   return [value, writeToLocalStorage]
 }
 
-export default useLocalStorage
+const useLocalStorageNoSync = key => {
+  const [value, setValue] = useState()
+
+  const updateValue = oldValue => {
+    const newValue = localStorage.getItem(key)
+    newValue !== oldValue ? setValue(newValue) : ''
+    return newValue
+  }
+
+  const write = newValue => {
+    if (newValue !== undefined) {
+      localStorage.setItem(key, newValue)
+    }
+    else {
+      localStorage.removeItem(key)
+    }
+    setValue(newValue)
+  }
+
+  // taken from https://stackoverflow.com/questions/11214404/how-to-detect-if-browser-supports-html5-local-storage
+  const localStorageEnabled = test => {
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+  }
+
+  // Function factory for creating curried functions. Does it make any sense?
+  const useStore = (...fns) => {
+    if (localStorageEnabled()) {
+      fns.reduce((f, g) => (...args) => f(g(...args)))
+    }
+  }
+
+  useEffect(() => useStore())
+
+  return [() => updateValue(value), useStore(updateValue, write)]
+}
+
+export default {useLocalStorage, useLocalStorageNoSync}
