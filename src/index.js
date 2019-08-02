98a930cb471ea46f0a3b8e7290eadeb3c6186715
Aleksander <aleksander.nowak@agillic.com>
2019-08-02 17:06:43 +0200

    Revert to being simple and smart

--- src/index.js
+++ src/index.js
@@ -3,50 +3,12 @@ import {useState, useEffect} from 'react'
 const useLocalStorage = (key, {updateFrequency}) => {
   const [value, setValue] = useState()
 
-  const readFromLocalStorage = () => {
+  const read = () => {
     const oldValue = value
     const newValue = localStorage.getItem(key)
     if (newValue !== oldValue) {
       setValue(newValue)
     }
-  }
-
-  const writeToLocalStorage = newValue => {
-    if (newValue !== undefined) {
-      localStorage.setItem(key, newValue)
-    }
-    else {
-      localStorage.removeItem(key)
-    }
-    setValue(newValue)
-  }
-
-  useEffect(() => {
-    let readLocalStorageIntervalId
-    if (window.localStorage) {
-      readFromLocalStorage()
-      readLocalStorageIntervalId = setInterval(
-        readFromLocalStorage,
-        updateFrequency
-      )
-    }
-    return () => {
-      if (window.localStorage) {
-        clearInterval(readLocalStorageIntervalId)
-      }
-    }
-  }, [])
-
-  return [value, writeToLocalStorage]
-}
-
-const useLocalStorageNoSync = key => {
-  const [value, setValue] = useState()
-
-  const updateValue = oldValue => {
-    const newValue = localStorage.getItem(key)
-    newValue !== oldValue ? setValue(newValue) : ''
-    return newValue
   }
 
   const write = newValue => {
@@ -59,27 +21,47 @@ const useLocalStorageNoSync = key => {
     setValue(newValue)
   }
 
-  // taken from https://stackoverflow.com/questions/11214404/how-to-detect-if-browser-supports-html5-local-storage
-  const localStorageEnabled = test => {
-    try {
-        localStorage.setItem(test, test);
-        localStorage.removeItem(test);
-        return true;
-    } catch(e) {
-        return false;
+  useEffect(() => {
+    let readLocalStorageIntervalId
+    if (window.localStorage) {
+      read()
+      readLocalStorageIntervalId = setInterval(
+        read,
+        updateFrequency
+      )
+    }
+    return () => {
+      if (window.localStorage) {
+        clearInterval(readLocalStorageIntervalId)
+      }
+    }
+  }, [])
+
+  return [value, write]
+}
+
+const useLocalStorageNoSync = key => {
+  const [value, setValue] = useState()
+
+  const read = () => {
+    const oldValue = value
+    const newValue = localStorage.getItem(key)
+    if (newValue !== oldValue) {
+      setValue(newValue)
     }
   }
 
-  // Function factory for creating curried functions. Does it make any sense?
-  const useStore = (...fns) => {
-    if (localStorageEnabled()) {
-      fns.reduce((f, g) => (...args) => f(g(...args)))
+  const write = newValue => {
+    if (newValue !== undefined) {
+      localStorage.setItem(key, newValue)
     }
+    else {
+      localStorage.removeItem(key)
+    }
+    setValue(newValue)
   }
 
-  useEffect(() => useStore())
-
-  return [() => updateValue(value), useStore(updateValue, write)]
+  return [read, write]
 }
 
 export default {useLocalStorage, useLocalStorageNoSync}
